3d80b0a3c8ef5abda85348c1fd8ca79d960068db
RealJohnGalt <johngalt@fake.mail>
2018-04-27 08:01:22 -0700

    repopick: set to cypheros

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -144,7 +144,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to LineageOS Gerrit
-    default_gerrit = 'https://review.lineageos.org'
+    default_gerrit = 'https://gerrit.cypheros.co'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
